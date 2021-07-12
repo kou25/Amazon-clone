e3d67fffbf49fa4b5c224bd197f09cb2f97986ab
Koustav <koustav@xenonstack.com>
2021-07-12 10:38:03 +0530

    export reducer

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -28,7 +28,7 @@ export const basketSlice = createSlice({
   },
 });
 
-export const { addToBasket, removeFromBasket } = basketSlice.actions;
+export const { addToBasket, removeFromBasket,clearBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
